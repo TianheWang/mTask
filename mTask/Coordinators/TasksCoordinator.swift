e142e402750d1f2d642f9063bb65922e3fee475a
Tianhe Wang <wang_tianhe@163.com>
2018-01-28 22:20:16 -0800

    google task API done

---
+++ mTask/Coordinators/TasksCoordinator.swift
@@ -0,0 +1,16 @@
+//
+//  TasksCoordinator.swift
+//  mTask
+//
+//  Created by Tianhe Wang on 1/28/18.
+//  Copyright © 2018 Tianhe Wang. All rights reserved.
+//
+
+import Foundation
+
+class TasksCoordinator {
+
+  func startFlow() {
+
+  }
+}
